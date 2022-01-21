60d85be4f9c622d617da936bb3a51fa934a012fb
Gabriel Cesarino <61214104+gabrielCesarino@users.noreply.github.com>
2022-01-21 08:17:24 -0300

    fix: formatting values on summary using Intl

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'; 
+import React, { useContext } from 'react';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
 import totalImg from '../../assets/total.svg'
@@ -7,14 +7,14 @@ import { TransactionsContext } from '../../TransactionsContext';
 import { Container } from "./styles";
 
 export function Summary() {
-    
+
     const { transactions } = useContext(TransactionsContext);
 
     const summary = transactions.reduce((acc, transaction) => {
-        if(transaction.type === 'deposit'){
+        if (transaction.type === 'deposit') {
             acc.deposits += transaction.amount;
             acc.total += transaction.amount;
-        }else {
+        } else {
             acc.withdraws += transaction.amount;
             acc.total -= transaction.amount;
         }
@@ -33,21 +33,36 @@ export function Summary() {
                     <p>Entradas</p>
                     <img src={incomeImg} alt="Entradas" />
                 </header>
-                <strong>{summary.deposits}</strong>
+                <strong>
+                    {new Intl.NumberFormat('pt-BR', {
+                        style: 'currency',
+                        currency: 'BRL',
+                    }).format(summary.deposits)}
+                </strong>
             </div>
             <div>
                 <header>
                     <p>Saídas</p>
                     <img src={outcomeImg} alt="Saídas" />
                 </header>
-                <strong> - {summary.withdraws}</strong>
+                <strong>-
+                    {new Intl.NumberFormat('pt-BR', {
+                        style: 'currency',
+                        currency: 'BRL',
+                    }).format(summary.withdraws)}
+                </strong>
             </div>
             <div className='highlight-background'>
                 <header>
                     <p>Total</p>
                     <img src={totalImg} alt="Total" />
                 </header>
-                <strong>{summary.total}</strong>
+                <strong>
+                    {new Intl.NumberFormat('pt-BR', {
+                        style: 'currency',
+                        currency: 'BRL',
+                    }).format(summary.total)}
+                </strong>
             </div>
         </Container>
     )
